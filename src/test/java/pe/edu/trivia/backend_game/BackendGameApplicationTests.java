a8cce1dc83e19b2d118a5b5105da7a408a624ecf
DevGitGabo <u21209578@utp.edu.pe>
2023-11-15 10:23:05 -0500

    init

---
+++ src/test/java/pe/edu/trivia/backend_game/BackendGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.trivia.backend_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
